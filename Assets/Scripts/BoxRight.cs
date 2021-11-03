239a6433a87c2964cf4db1a761c775526f70d538
Dave Mangold <dave@davemangold.com>
2021-11-02 21:34:23 -0700

    Initial working state
    Got the game back to a working state after migrating to git repo and refactoring code.

--- Assets/Scripts/BoxRight.cs
+++ Assets/Scripts/BoxRight.cs
@@ -2,20 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class BoxRight : MonoBehaviour
 {
 
+    public int maxCountables = 10;
     public Text targetText;
+    public Text countText;
     public Button plusButton;
+    private int targetValue;
+    private List<GameObject> countables;
+    private BoxCollider2D collider;
 
     void Start()
     {
-        
+        countables = new List<GameObject>();
+        collider = gameObject.GetComponent<BoxCollider2D>();
+        ResetTarget();
     }
 
-    void Update()
-    {
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Collider entered box.");
+        countables.Add(other.gameObject);
+        countText.text = countables.Count.ToString();
+        CheckStatus();
+    }
 
+    void OnTriggerExit2D(Collider2D other) {
+        Debug.Log("Collider exited box.");
+        countables.Remove(other.gameObject);
+        countText.text = countables.Count.ToString();
+        CheckStatus();
+    }
+
+    void CheckStatus() {
+
+        float waitTime = 1.5f;
+
+        if (countables.Count == targetValue) {
+            Debug.Log("Target value reached.");
+            plusButton.interactable = false;
+            Invoke("Celebrate", waitTime);
+        }
+    }
+
+    void Celebrate() {
+
+        float jumpSpeed = 250f;
+        float scaleFactor = 1.5f;
+        float waitTime = 1.8f;
+
+        foreach (GameObject countable in countables) {
+            countable.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpSpeed);
+        }
+
+        targetText.transform.localScale *= scaleFactor;
+        Invoke("RestartScene", waitTime);
+
+    }
+
+    void RestartScene() {
+        SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex );
+    }
+
+    void ResetTarget() {
+        targetValue = Random.Range(1, maxCountables + 1);
+        targetText.text = targetValue.ToString();
     }
 }
--- Assets/Scripts/PlusButton.cs
+++ Assets/Scripts/PlusButton.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class PlusButton : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject countableBox;
+    public GameObject countablePrefab;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+    private BoxCollider2D boxCollider;
+    private Button button;
+
     void Start()
     {
-        
+        button = gameObject.GetComponent<Button>();
+        boxCollider = countableBox.GetComponent<BoxCollider2D>();
+
+        button.onClick.AddListener(TaskOnClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    void TaskOnClick()
     {
-        
+        Debug.Log("Plus button clicked.");
+
+        float spawnSpeed = 250f;
+
+        GameObject countable = Instantiate(countablePrefab, countableBox.transform.position, Quaternion.identity);
+        countable.transform.eulerAngles = new Vector3(
+            countable.transform.eulerAngles.x,
+            countable.transform.eulerAngles.y,
+            Random.Range(-60, 60));
+        Vector2 force = new Vector2(countable.transform.up.x, countable.transform.up.y);
+        countable.GetComponent<Rigidbody2D>().AddForce(force.normalized * spawnSpeed);
+        SpawnSound();
+    }
+
+    void SpawnSound() {
+        audioSource.PlayOneShot(audioClip);
     }
 }
